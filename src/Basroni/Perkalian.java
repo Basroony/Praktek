192e8115e6d87c0819960a046c4e55bb07bcc140
SBC4 <SBC4@SBC13>
2015-04-28 14:01:25 +0700

    Commit Perkalian

---
+++ src/Basroni/Perkalian.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Basroni;
+
+/**
+ *
+ * @author SBC4
+ */
+public class Perkalian {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 15;
+        int kali = a * b;
+        System.out.println(a+" x "+b+" = "+kali);
+    }
+}
